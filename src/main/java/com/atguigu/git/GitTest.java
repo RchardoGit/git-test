39b3126c3a6042982115a01e19c53e060e538ee6
Kly <kly@qq.com>
2021-11-03 15:35:52 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author KongLingyang
+ * @since 2021-11-2
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
